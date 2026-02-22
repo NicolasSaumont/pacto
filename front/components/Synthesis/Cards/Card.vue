95b25c43797705767cdbc5c7e963c38d26db67c3
Nicolas Saumont <saumont.nicolas@gmail.com>
2026-02-22 09:20:27 +0100

    feat: cards

---
+++ front/components/Synthesis/Cards/Card.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+defineProps<{
+  count: number
+  description: string
+}>()
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 bg-gray-900 border border-gray-600 rounded-2xl p-6 w-full">
+    <span class="text-4xl font-bold">{{ count }}</span>
+    <span class="text-xl text-gray-400">{{ description }}</span>
+  </div>
+</template>
---
+++ front/components/Synthesis/Cards/Cards.vue
@@ -0,0 +1,9 @@
+<script setup lang='ts'></script>
+
+<template>
+  <div class="flex gap-10">
+    <SynthesisCardsCard :count="12" description="Commandes" />
+    <SynthesisCardsCard :count="8" description="Clients" />
+    <SynthesisCardsCard :count="92" description="Produits" />
+  </div>
+</template>
